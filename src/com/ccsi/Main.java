9814dfc7fda19ad0331398557e546bf69f119e45
liugxiami <xiamiliu@gmail.com>
2016-10-09 15:43:38 -0500

    Update Main.java

--- src/com/ccsi/Main.java
+++ src/com/ccsi/Main.java
@@ -20,6 +20,7 @@ public class Main {
 //push(x)--push element x onto stack.
 //pop()--removes the element on top of the stack
 //empty()--Return whether the stack is empty.
+//wrong method.
 class MyStack{
     Queue<Integer> myQueue=new LinkedList<>();
     Queue<Integer> helper=new LinkedList<>();
@@ -40,6 +41,39 @@ class MyStack{
             helper.offer(myQueue.poll());
         }
         int res=helper.peek();
+        while(!helper.isEmpty()){
+            myQueue.offer(helper.poll());
+        }
+        return res;
+    }
+    public  boolean empty(){
+        return myQueue.isEmpty();
+    }
+}
+//modified
+class MyStack{
+    Queue<Integer> myQueue=new LinkedList<>();
+    Queue<Integer> helper=new LinkedList<>();
+    public void push(int x){
+        myQueue.offer(x);
+    }
+    public void pop(){
+        int size=myQueue.size();
+        for (int i = 0; i < size-1; i++) {
+            helper.offer(myQueue.poll());
+        }
+        myQueue.poll();
+        while (!helper.isEmpty()){
+            myQueue.offer(helper.poll());
+        }
+    }
+    public int top(){
+        int size=myQueue.size();
+        for (int i = 0; i < size-1; i++) {
+            helper.offer(myQueue.poll());
+        }
+        int res=myQueue.peek();
+        helper.offer(myQueue.poll());
         while(!helper.isEmpty()){
             myQueue.offer(helper.poll());
         }
